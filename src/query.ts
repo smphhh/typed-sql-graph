489f2fabe5fd78675f719a94c6a372ae833db024
BigDataSamuli <samuli.holopainen@bigdatasolutions.fi>
2016-11-07 22:37:43 +0200

    Allow creating join query with transaction mapper

--- src/query.ts
+++ src/query.ts
@@ -1,11 +1,12 @@
 
 import { Mapper, WrappedMappingData } from 'simple-typed-sql';
+import { BaseMapper } from 'simple-typed-sql/lib/core';
 
 import { FromQuery } from 'simple-typed-sql/lib/core';
 
 import { JoinPath } from './sql_graph';
 
-export function createJoinQuery(mapper: Mapper, joinPath: JoinPath) {
+export function createJoinQuery(mapper: BaseMapper, joinPath: JoinPath) {
     return joinPath.reduce((query, join) => {
         return query.innerJoin(join.detailMapping, join.joinCondition);
 
